6d0af38eb2cf6aea0b3c723b38db2b4246a832ee
Muhammad Ali Rashid <ali.rashed@live.com>
2020-05-21 14:44:36 +0500

    Using Material UI to modify Theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,58 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import Logo from '../images/logo.png'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+// import styled from 'styled-components';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+// import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+// import Button from '@material-ui/core/Button';
+// import NoSsr from '@material-ui/core/NoSsr';
 
-const Navbar = () => {
-	let imageStyle = {
-		height: '40px'
-	}
-	return (
-		<nav className = "navbar navbar-dark bg-primary navbar-expand-lg" >
+const muiTabStyles = makeStyles({
+	textColorInherit: {
+    background:'blue',
+    '&.Mui-selected':{border:'2px solid green'}
+  },
+}, { name: 'MuiTab' });
+
+
+function Navbar() {
+  const muiTabStyled = muiTabStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+		console.log(value);
+  };
+
+  
+
+  return (
+    <div>
+			<AppBar position="static">
 			<div className="container">
-				<NavLink className="navbar-brand" to="/">
-					<img src={Logo} alt="Crypto Info" style={imageStyle} />CrypTo inFo
-				</NavLink>
-				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav">
-						<li className="nav-item">
-							<NavLink className="nav-link" to="/">Home</NavLink>
-						</li>
-						<li className="nav-item">
-							<NavLink className="nav-link" to="/top/1/10">Top 100</NavLink>
-						</li>
-					</ul>
-				</div>
+				<Toolbar variant="dense">
+						<Tabs
+			        value={value}
+			        onChange={handleChange}
+			        centered
+			      >
+			        <Tab label="Item One" />
+			        <Tab label="Item Two" />
+			        <Tab label="Item Three" />
+			      </Tabs>
+				</Toolbar>
 			</div>
-		</nav>
-	)
+      </AppBar>
+    </div>
+  );
 }
 
 export default Navbar
+
+// <NavLink className="nav-link" to="/">Home</NavLink>
+// <NavLink className="nav-link" to="/top/1/10">Top 100</NavLink>
